79e27c06ca99a9af53945b50cfbad3a736c0d209
Patrick Brennan <pbrennanmusic@gmail.com>
2021-08-02 01:03:26 -0400

    commit tree ef1d15d8fcfce79ee244c43be4c1180dea9fba5e

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,15 +3,19 @@ class Review < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
 
   validates :rating, :description, presence: true
-  validates :reservation, presence: true, if: [Proc.new {|r| r.reservation.present?}, :checked_out, :status?]
+  validates :reservation, presence: true
+  validate :checked_out?
+  validate :status?
 
-  def checked_out
+  def checked_out?
+    return if self.reservation.nil?
     if self.reservation.checkout >= Date.today
       errors.add(:reservation, "You have not checked out yet")
     end
   end
 
   def status?
+    return if self.reservation.nil?
     if self.reservation.status != "accepted"
       errors.add(:reservation, "You reservation has not been accepted as of now.")
     end
